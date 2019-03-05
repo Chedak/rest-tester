ddf540d5f5c50edf020bed0d5f8a58d0ffc7072a
Chedak <cedomir.miskov@gmail.com>
2019-03-05 16:19:50 +0100

    resolve errors

--- app/script/rest_requestor.rb
+++ app/script/rest_requestor.rb
@@ -1,7 +1,8 @@
 require 'rest-client'
 
-puts RestClient.get("http://localhost:3001/users")
-puts RestClient.get("http://localhost:3001/users/new")
-puts RestClient.get("http://localhost:3001/users/2")
-puts RestClient.get("http://localhost:3001/users/3/edit")
-puts RestClient.post("http://localhost:3001/users", "")
+
+puts RestClient.get "http://localhost:3000/users"
+puts RestClient.get "http://localhost:3000/users/new"
+puts RestClient.get "http://localhost:3000/users/2"
+puts RestClient.get "http://localhost:3000/users/3/edit"
+puts RestClient.post "http://localhost:3000/users", ''
